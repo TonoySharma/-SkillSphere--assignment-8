f807517f3d990b7b29bfe012f1a9876eaf19a5b9
Your Name <you@example.com>
2026-04-30 01:40:56 +0600

    added all photos page

--- src/app/all-courses/page.jsx
+++ src/app/all-courses/page.jsx
@@ -1,11 +1,25 @@
+import PhotoCard from '@/components/photocard/PhotoCard';
 import React from 'react';
 
-const CoursesPage = () => {
+const AllCoursesPage = async () => {
+      const res = await fetch('https://skill-sphere-assignment-8.vercel.app/data.json');
+  const photos = await res.json();
+//   console.log(photos, 'photos');
+  
     return (
-        <div>
-            <h1>courses page</h1>
+        <div className=''>
+            <h1 className='text-center my-5 text-3xl font-bold'>🔥 Popular Courses</h1>
+
+            <div className='max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+                {
+                    photos.map(photo => <PhotoCard key={photo.ind} photo={photo}>
+
+                    </PhotoCard>
+
+                    )}
+            </div>
         </div>
     );
 };
 
-export default CoursesPage;
+export default AllCoursesPage;
